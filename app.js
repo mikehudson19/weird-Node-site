76cf59ae02ec22627b5d0e3b17d572678f5c3191
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-24 09:54:37 +0200

    Add header and footer to layout page with basic styling.

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 
-
 app = express();
 bodyParser.urlencoded({extended : true});
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static('public'))
 
 
-
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 
 const PORT = process.env.PORT || 3000;
