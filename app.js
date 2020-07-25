3a1ca0f931440e931b76dbf970c859479eeae1a7
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-25 14:00:32 +0200

    Add task list basic functionality.

--- app.js
+++ app.js
@@ -11,18 +11,16 @@ app.use(express.static('public'))
 const posts = [{ 
   blogTitle: 'Title 1',
   blogContent: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
-},
-{ 
-  blogTitle: 'Title 2',
-  blogContent: 'THis is the secnd fucking post, bitch. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
 }];
+
+const tasks = [];
 
 app.get('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/tasks', (req, res) => {
-  
+  res.render('tasks', {tasks : tasks});
 })
 
 app.get('/blog', (req, res) => {
@@ -51,6 +49,15 @@ app.post('/compose', (req, res) => {
   res.redirect('/blog');
 })
 
+app.post('/tasks', (req, res) => {
+  console.log(req.body);
+  const task = req.body.task;
+  tasks.push(task);
+  res.redirect('/tasks');
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { console.log('Server is running on port 3000...' )})
+
+
